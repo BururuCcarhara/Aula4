10ea25c9a5d804610ced86f1a0211fa1d9022791
Inty <lkillaburuz@gmail.com>
2024-08-14 16:17:35 -0300

    Aula4

---
+++ Cliente.java
@@ -0,0 +1,44 @@
+package Lore;
+
+public class Cliente {
+	 
+	    
+	 int id;
+	 String nome;
+	 int telefone;
+	 int cpf;
+	 int RG;
+	 
+	 
+	 public int getId() {
+	        return id;
+	    }
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	    public String getNome() {
+	        return nome;
+	    }
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+	    public int getCpf() {
+	        return cpf;
+	    }
+	    public void setCpf(int cpf) {
+	        this.cpf = cpf;
+	    }
+	    public int getRg() {
+	        return RG;
+	    }
+	    public void setRg(int RG) {
+	        this.RG = RG;
+	    }
+	    public int getTelefone() {
+	        return telefone;
+	    }
+	    public void setTelefone(int telefone) {
+	        this.telefone = telefone;
+	    }
+	
+}
---
+++ ObjetoCliente.java
@@ -0,0 +1,10 @@
+package Lore;
+
+public class ObjetoCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
